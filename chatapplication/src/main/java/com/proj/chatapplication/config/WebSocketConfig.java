46d932c4db468e56a853db632702f646addab6a9
mohamedumar07 <bilalumar56789@gmail.com>
2025-02-05 23:47:26 +0530

    initial commit

---
+++ chatapplication/src/main/java/com/proj/chatapplication/config/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package com.proj.chatapplication.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+
+    //register websocket endpoint
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry)
+    {
+        registry.addEndpoint("/chat")
+                .setAllowedOrigins("*") //origin, client address(react)
+                .withSockJS();
+    }
+
+    //configure message broker
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry)
+    {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
---
+++ chatapplication/src/test/java/com/proj/chatapplication/ChatapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.chatapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
